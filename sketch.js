a255f292bc30604389942c8351ff7dc55c30b975
Suhail Ahamed <suhailahamed@Suhails-MacBook-Pro.local>
2018-05-08 18:00:31 +0400

    First Commit, working on PC

---
+++ sketch.js
@@ -0,0 +1,16 @@
+//Suhail's First AR Project
+
+var myWidth = 600, myHeight = 400;
+let c1 = [];
+
+
+function setup() {
+	//createCanvas(windowWidth, windowHeight);
+	noCanvas()
+
+
+}
+
+function draw() {
+
+}
